6190a880fd78032b1c1d0431bc6de0d4f3bd8214
Deeksha K <deeksha.23cs033@sode-edu.in>
2026-05-18 16:25:52 +0530

    Button update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,53 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-  darkMode: ['class'],
+export default {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      
-      },
-    },
     extend: {
       colors: {
-        'primary-500': '#877EFF',
-        'primary-600': '#5D5FEF',
-        'secondary-500': '#FFB620',
-        'off-white': '#D0DFFF',
-        'red': '#FF5A5A',
-        'dark-1': '#000000',
-        'dark-2': '#09090A',
-        'dark-3': '#101012',
-        'dark-4': '#1F1F22',
-        'light-1': '#FFFFFF',
-        'light-2': '#EFEFEF',
-        'light-3': '#7878A3',
-        'light-4': '#5C5C7B',
-      },
-      screens: {
-        'xs': '480px',
-      
-      },
-      width: {
-        '420': '420px',
-        '465': '465px',
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
 
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        primary: {
+          DEFAULT: "#877EFF",
+          500: "#877EFF",
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+
+        light: {
+          1: "#FFFFFF",
+          2: "#EFEFEF",
+          3: "#7878A3",
+          4: "#5C5C7B",
+        },
+
+        dark: {
+          1: "#000000",
+          2: "#09090A",
+          3: "#101012",
+          4: "#1F1F22",
         },
       },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+
+      screens: {
+        xs: "480px",
+      },
+
+      width: {
+        465: "465px",
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 };
