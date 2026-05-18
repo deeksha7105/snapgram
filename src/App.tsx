e03cba2fedf1a1b17fbb3b5f2782f5f6e68219d8
kartiksy-cmyk <kartiksy9@gmail.com>
2026-05-18 16:55:58 +0530

    updated tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,52 @@
-import {Route, Routes} from 'react-router-dom';
+import { memo } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
-import './global.css';
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
 
+import "./globals.css";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/*public routes */}
-        <Route element={<AuthLayout/>}>
-          <Route path="/sign-in" element={<SigninForm/>} />
-          <Route path="/sign-up" element={<SignupForm/>} />
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
-        {/*private routes */}
-        <Route element={<RootLayout/>}>
-         <Route index element={<Home/>}/>
-        </Route>           
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+        </Route>
       </Routes>
-    </main>
-  )
-}
 
-export default App
+      <Toaster />
+    </main>
+  );
+};
+
+export default memo(App);
